7418e647443c6430e2a9785f4101cbf78107fa17
SokolowskiLukasz <lukasz.m.sokolowski1995@gmail.com>
2019-03-20 21:44:35 +0100

    Add text boxes for user input

--- Grafika/Form1.cs
+++ Grafika/Form1.cs
@@ -41,5 +41,15 @@ namespace Grafika {
         private void Form1_Load(object sender, EventArgs e) {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
